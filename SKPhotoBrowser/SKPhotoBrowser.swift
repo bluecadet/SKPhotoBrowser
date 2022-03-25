bc008dea314efba3ae0201adf125e6b83c516f83
Benjamin Bojko <benjaminbojko@users.noreply.github.com>
2022-03-25 11:52:49 -0400

    Removed use of UI_USER_INTERFACE_IDIUM

--- SKPhotoBrowser/SKPhotoBrowser.swift
+++ SKPhotoBrowser/SKPhotoBrowser.swift
@@ -249,7 +249,7 @@ open class SKPhotoBrowser: UIViewController {
             self.hideControlsAfterDelay()
             self.activityViewController = nil
         }
-        if UI_USER_INTERFACE_IDIOM() == .phone {
+        if SKMesurement.isPhone {
             present(activityViewController, animated: true, completion: nil)
         } else {
             activityViewController.modalPresentationStyle = .popover
@@ -491,7 +491,7 @@ internal extension SKPhotoBrowser {
                 }))
             }
             
-            if UI_USER_INTERFACE_IDIOM() == .phone {
+            if SKMesurement.isPhone {
                 present(actionSheetController, animated: true, completion: nil)
             } else {
                 actionSheetController.modalPresentationStyle = .popover
